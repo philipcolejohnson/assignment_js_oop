5baf89ec4865f04429e5ad44b1c71f83da0f2281
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-13 09:28:53 -0700

    Warmups

---
+++ script.js
@@ -0,0 +1,43 @@
+function Asteroid(x, y, velX, velY) {
+  this.x = x || 0;
+  this.y = y || 0;
+  this.velX = velX || 0;
+  this.velY = velY || 0;
+}
+
+Asteroid.prototype.tic = function() {
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// var ast1 = new Asteroid(20, 25, 5, 10);
+// var ast2 = new Asteroid(15, 20, 3, 6);
+
+// console.log(ast1);
+// console.log(ast2);
+
+// ast1.tic();
+// ast2.tic();
+
+// console.log(ast1);
+// console.log(ast2);
+
+var asteroids = [];
+for (var i = 0; i < 1000000; i++) {
+    var x = Math.random() * 50;
+    var y = Math.random() * 50;
+    var velX = Math.random() * 50;
+    var velY = Math.random() * 50;
+    asteroids.push(new Asteroid(x, y, velX, velY) );
+}
+
+var start = new Date();
+
+for (var i = 0; i < asteroids.length; i++ ) {
+  asteroids[i].tic();
+}
+
+var end = new Date();
+
+console.log( end.getTime() - start.getTime() );
+
